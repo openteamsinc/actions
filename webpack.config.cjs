dad16a0cad49aaa409844cd1a5e8aa4ad11c2c17
Sean Ross-Ross <srossross@gmail.com>
2024-09-23 09:08:31 -0700

    use proper ext

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,9 +4,13 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "index.js",
+    // eslint-disable-next-line no-undef
     path: path.resolve(__dirname, "dist"),
     libraryTarget: "commonjs2",
   },
   target: "node",
   mode: "production",
+  optimization: {
+    minimize: false,
+  },
 };
